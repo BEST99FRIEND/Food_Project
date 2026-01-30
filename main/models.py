00780a234e9d7d371fdce3eb6f9c6b763467760a
BEST99FRIEND <asadovahror9@gmail.com>
2026-01-30 16:12:05 +0500

    1

--- main/models.py
+++ main/models.py
@@ -10,3 +10,25 @@ class HeroSlider(models.Model):
 
     def __str__(self):
         return self.title
+    
+
+class Category(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+    
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='images/products/', null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    discount = models.IntegerField(default=0)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
