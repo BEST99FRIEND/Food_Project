fed2f59965a57f7ec42db04eb241274f6d632003
BEST99FRIEND <asadovahror9@gmail.com>
2026-02-09 10:31:35 +0500

    1

--- user/urls.py
+++ user/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('register/', views.register_view, name='register'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
-    path('profile/<int:user_id>/', views.user_profile, name='profile'),
+    path('profile/<str:username>/', views.user_profile, name='profile'),
 ]
--- user/views.py
+++ user/views.py
@@ -37,10 +37,10 @@ def logout_view(request: WSGIRequest):
     print('Siz tizimdan chiqdingiz')
     return redirect('login')
 
-def user_profile(request: WSGIRequest, user_id: int):
-    # user = get_object_or_404(User, id=user_id)
+def user_profile(request: WSGIRequest, username: str):
+    user = get_object_or_404(User, username=username)
     context = {
-        
+        "user": user
     }
     return render(request, template_name='user/profile.html', context=context)
 
